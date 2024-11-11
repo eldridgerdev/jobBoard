663eff985fe67655278963b6587357d0a20025f6
Ryan Eldridge <eldridgerdev@gmail.com>
2024-11-11 16:57:33 -0500

    A little bit of API documentation

--- app/services/getJobs.ts
+++ app/services/getJobs.ts
@@ -3,21 +3,33 @@ import { Job } from "../jobs/types";
 const JOBS_API_HOST = "jobs-api14.p.rapidapi.com";
 const API_KEY = process.env.NEXT_PUBLIC_RAPID_API_KEY;
 
+/** https://rapidapi.com/Pat92/api/jobs-api14/playground/apiendpoint_90c56edf-c5a6-45d6-b5a9-686ad050a25a */
+interface JobsApiOptions {
+  /** Required Keywords, Jobtitle, Company name or any other relevant search-query. */
+  query: string;
+  /** Required City, country, or any other locations. */
+  location: string;
+  /** Optional Default value = -1.0 Distance to the location in kilometers */
+  distance: number;
+}
+
+const data: JobsApiOptions = {
+  query: "Web Developer",
+  location: "United States",
+  distance: 1,
+};
+
 const opts = {
   method: "GET",
   url: `https://${JOBS_API_HOST}`,
-  data: {
-    keywords: "Web Developer",
-    location: "United States",
-    distance: 1,
-  },
+  data,
   headers: {
     "x-rapidapi-host": JOBS_API_HOST,
     "x-rapidapi-key": API_KEY,
   },
 };
 
-const query = `query=${opts.data.keywords}`;
+const query = `query=${opts.data.query}`;
 const location = `&location=${opts.data.location}`;
 const distance = `&distance=${opts.data.distance}`;
 const language = `&language=en_US`;
