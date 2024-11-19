68ca0e821952d7f733f28a83faa35a94edb30bdd
Ryan <eldridgerdev@gmail.com>
2024-11-19 13:01:24 -0500

    remove hydration workaround, this was fixed

--- app/jobs/JobCardList.tsx
+++ app/jobs/JobCardList.tsx
@@ -1,21 +1,9 @@
 "use client";
 import { Grid2 } from "@mui/material";
 import { Job } from "./types";
-import { useEffect, useState } from "react";
 import JobCard from "./JobCard";
 
 export default function JobCardList({ jobs }: { jobs: Job[] | [] }) {
-  const [hydrated, setHydrated] = useState(false);
-
-  // useEffect is called after hydration,
-  //   use this to render JobCards after SSR hydration because they are dynamic
-  //   @TODO: Look into suspense
-  useEffect(() => {
-    setHydrated(true);
-  }, []);
-  if (!hydrated) {
-    return null;
-  }
   return (
     <Grid2 container spacing={2}>
       {jobs.map((job) => (
